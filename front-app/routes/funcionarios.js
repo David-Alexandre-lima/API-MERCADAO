18a5203b3d17c2c21138507877490f8257475035
Carlos Costa <132078528+CarlosHoland4@users.noreply.github.com>
2024-06-07 23:40:42 +0000

    modifiquei a lista de funcionarios inseri materialize

--- front-app/routes/funcionarios.js
+++ front-app/routes/funcionarios.js
@@ -5,7 +5,7 @@ const cols = ["id", "nome", "email", "cargo", "sexo"]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    fetch("https://fuzzy-garbanzo-pvxq5w4v796hrpr5-3000.app.github.dev/funcionarios")
+    fetch("https://redesigned-space-xylophone-g45x67pw7v52vvv6-3000.app.github.dev/funcionarios")
     .then((res)=>{return res.json()})
     .then((funcionarios)=>{
         res.render('funcionario', { title: 'Lista de Funcionarios', funcionarios: funcionarios, cols: cols });
--- front-app/routes/usuarios.js
+++ front-app/routes/usuarios.js
@@ -5,7 +5,7 @@ const cols = ["id", "nome", "email", "cpf"]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    fetch("https://cuddly-space-chainsaw-w6wjq5p67x4cg6j5-3000.app.github.dev/usuarios")
+    fetch("https://redesigned-space-xylophone-g45x67pw7v52vvv6-3000.app.github.dev//usuarios")
     .then((res)=>{return res.json()})
     .then((usuarios)=>{
         res.render('usuario', { title: 'Lista de Usuarios', usuarios: usuarios, cols: cols });
